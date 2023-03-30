5c8421ca4c2852a739812436ec4a5a3eed80bdb0
Maria Garcia <mgarcia18@hawk.iit.edu>
2023-03-29 19:07:06 -0500

    cleaned up and fixed typos

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -13,14 +13,20 @@ const initialState = {
 }
 
 //Register User
-export const register = createAsyncThunk('auth/register', async(user, thunkAPI) => {
-    try {
-        return await authService.register(user)
-    } catch (error) {
-        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
-        return thunkAPI.rejectWithValue(message)
+export const register = createAsyncThunk('auth/register', 
+    async(user, thunkAPI) => {
+        try {
+            return await authService.register(user)
+        } catch (error) {
+            const message = 
+                (error.response && 
+                    error.response.data && 
+                    error.response.data.message) || 
+                error.message || error.toString()
+            return thunkAPI.rejectWithValue(message)
+        }
     }
-})
+)
 
 export const authSlice = createSlice({
     name: 'auth',
@@ -32,13 +38,13 @@ export const authSlice = createSlice({
             state.isSuccess = false
             state.isError = false
             state.message = ''
-        }
+        },
     },
     extraReducers: (builder) => {
         builder
-        .addCase(register.pending, (state => {
+        .addCase(register.pending, (state) => {
             state.isLoading = true
-        }))
+        })
         .addCase(register.fulfilled, (state, action) => {
             state.isLoading = false
             state.isSuccess = true
@@ -50,7 +56,7 @@ export const authSlice = createSlice({
             state.message = action.payload
             state.user = null
         })
-    }
+    },
 })
 
 export const {reset} = authSlice.actions
